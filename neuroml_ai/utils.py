d7bb88b0fd4e4a14229570367ef363cf8f02b25d
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-01-07 18:41:59 +0000

    feat(streamlit): update to use httpx

--- neuroml_ai/utils.py
+++ neuroml_ai/utils.py
@@ -13,14 +13,16 @@ from tenacity import (
     retry,
     retry_if_exception_type,
     stop_after_attempt,
-    wait_random_exponential
+    wait_random_exponential,
 )
 
 
 @retry(
-    wait=wait_random_exponential(60),
+    wait=wait_random_exponential(multiplier=1, max=10),
     stop=stop_after_attempt(10),
-    retry=retry_if_exception_type((httpx.ConnectError, httpx.HTTPStatusError)),
+    retry=retry_if_exception_type(
+        (httpx.ConnectError, httpx.HTTPStatusError, httpx.ReadError, httpx.ReadTimeout)
+    ),
     reraise=True,
 )
 async def check_api_is_ready():
