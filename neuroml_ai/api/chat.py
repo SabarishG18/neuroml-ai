11bd21bd0b9e95c2e2dd0373465791933ae0c4b4
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-01-21 14:52:28 +0000

    chore: reformat

--- neuroml_ai/api/chat.py
+++ neuroml_ai/api/chat.py
@@ -8,13 +8,12 @@ Copyright 2025 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
-
 from fastapi import APIRouter, Request
 
 chat_router = APIRouter()
 
+
 @chat_router.post("/query")
 async def query(request: Request, query: str):
-
     rag = request.app.state.rag
     return {"result": await rag.run_graph_invoke(query)}
