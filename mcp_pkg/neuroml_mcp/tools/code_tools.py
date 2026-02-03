e02c5779a859194ceffd5bdc3ace47449a56ab50
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-02-03 15:09:30 +0000

    feat(mcp): add file lister tool

--- mcp_pkg/neuroml_mcp/tools/code_tools.py
+++ mcp_pkg/neuroml_mcp/tools/code_tools.py
@@ -10,8 +10,9 @@ Copyright 2025 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
-from typing import Any, Dict, List
 from dataclasses import asdict
+from pathlib import Path
+from typing import Any, Dict, List, Optional
 
 from neuroml_mcp.tools.sandbox import docker, local
 from neuroml_mcp.tools.sandbox.sandbox import RunCommand, RunPythonCode
@@ -33,31 +34,79 @@ async def dummy_code_tool(astring: str) -> str:
     return f"I got {astring}"
 
 
-async def execute_command_tool(command: str) -> Dict[str, Any]:
-    """Execute a command in a shell.
+async def list_files_tool(
+    path: str,
+    max_depth: Optional[int] = None,
+    pattern: Optional[str] = None,
+    include_files: bool = True,
+    include_directories: bool = True,
+    recursive: bool = False,
+    limit: int = 100,
+) -> Dict[str, Any]:
+    """List files and directories in a given path
 
     Input:
 
-    - command (string):
-      Example: "ls -l -A"
+    - a path (string): path relative to current root. Must not include ".."
+    - max_depth (integer): maximum number of levels
+    - pattern (string): pattern to match, passed to Python's Path.glob function.
+    - include_files (bool, default True): include files in list
+    - include_directories (bool, default True): include directories in list
+    - recursive (bool, default False): if True, traverse subdirectories
+    - limit (integer, default 100): maximum number of entries returned
 
     Output:
 
     Dictionary with keys:
-    - stdout (str)
-    - stderr (str)
-    - returncode (int)
-    - data (dict): additional metadata
+
+    - files (list of dictionaries): list of returned files with metadata:
+        - path: full file path
+        - type: whether it is a file or a directory
+        - modified time: time file was last modified
+        - size: size of file
+    - error (str): error if any
+    - truncated (bool): True if list was limited to `limit` entries
 
     Examples:
-    - execute_command_tool(command="ls")
-    - execute_command_tool(command="ls -l"])
+
+    - list all files and folders in current directory: list_files_tool(path=".")
+    - list only python files in "source" directory: list_files_tool(path="./source/", pattern="*.py")
 
     """
-    request = RunCommand(command=command.split())
-    async with sbox(".") as f:
-        result = await f.run(request)
-    return asdict(result)
+    the_path = Path(path)
+
+    error = ""
+    files = []
+
+    if ".." in path:
+        return {
+            "files": [],
+            "truncated": False,
+            "error": "Path contains '..', exiting.",
+        }
+
+    try:
+        if pattern is not None:
+            for f in the_path.glob(pattern):
+                ftype = "file"
+                if f.is_dir():
+                    ftype = "directory"
+                if f.is_symlink():
+                    ftype = "link"
+                files.append(
+                    {
+                        "path": str(f),
+                        "type": ftype,
+                        "modified time": f.stat().st_mtime,
+                        "size": f.stat().st_size,
+                    }
+                )
+    except Exception as e:
+        error = e.__str__()
+
+    result = {"files": files, "error": error}
+
+    return result
 
 
 async def run_python_code_tool(code: str) -> Dict[str, Any]:
