1fbbc26373e937e1ee4c2be87573616624bdb9ff
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-02-04 14:35:14 +0000

    chore: add hh tools module

---
+++ mcp_pkg/neuroml_mcp/tools/hh_tools.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Tools specific to Open Worm HH simulation
+
+File: mcp_pkg/neuroml_mcp/tools/hh_tools.py
+
+Copyright 2025 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
